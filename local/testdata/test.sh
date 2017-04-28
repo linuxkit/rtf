04daa165121796444bf4ab859cb1e2118d999854
Rolf Neugebauer <rolf.neugebauer@docker.com>
2017-04-28 18:22:44 +0100

    Fix shocking typos
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- local/testdata/test.sh
+++ local/testdata/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # AUTHOR: Dave Tucker <dt@docker.com>
-# AUTHOR: Rolf Neugebauer <rofl.neugebauer@docker.com>
+# AUTHOR: Rolf Neugebauer <rolf.neugebauer@docker.com>
 # SUMMARY: A Test
 # LABELS: foo, bar, !baz
 # REPEAT: 5
