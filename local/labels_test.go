2289c26d3966fd43ed12ab5d57f95e8d7b09d01c
Dave Tucker <dt@docker.com>
2017-06-07 16:56:11 +0100

    Change label check logic to look for only one match
    Not all labels given to a test are required to be present.
    Only one is required.
    Add tests to ensure this is honoured.
    
    Signed-off-by: Dave Tucker <dt@docker.com>

--- local/labels_test.go
+++ local/labels_test.go
@@ -19,3 +19,37 @@ func TestParseLabels(t *testing.T) {
 		t.Fatalf("\nExpected %+v\nGot: %+v\n", expectedUnSet, unSet)
 	}
 }
+
+func TestWillRun(t *testing.T) {
+
+	darwin := map[string]bool{"darwin": true}
+	linux := map[string]bool{"linux": true}
+	both := map[string]bool{"darwin": true, "linux": true}
+
+	darwinUser := RunConfig{Labels: darwin}
+	linuxUser := RunConfig{Labels: linux}
+
+	if !WillRun(nil, nil, RunConfig{}) {
+		t.Fatalf("Test with no labels on host with no labels doesn't run")
+	}
+
+	if !WillRun(nil, nil, darwinUser) {
+		t.Fatalf("Test with no labels doesn't run!")
+	}
+
+	if !WillRun(darwin, nil, darwinUser) {
+		t.Fatalf("Darwin test doesn't run for darwin user")
+	}
+
+	if !WillRun(linux, nil, linuxUser) {
+		t.Fatalf("Linux test doesn't run for linux user")
+	}
+
+	if WillRun(darwin, nil, linuxUser) {
+		t.Fatalf("Darwin test runs for linuxUser")
+	}
+
+	if !WillRun(both, nil, darwinUser) {
+		t.Fatalf("Test on darwin/linux doesn't run for darwin user")
+	}
+}
