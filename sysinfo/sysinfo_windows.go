92e9aef9c229df0facded939051f86c33cc34431
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-04-09 17:20:43 +0100

    sysinfo: Add CPU, Memory, and Model to the system information
    These are useful for reporting test results and for benchmarking.
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- sysinfo/sysinfo_windows.go
+++ sysinfo/sysinfo_windows.go
@@ -6,6 +6,12 @@ import (
 )
 
 func getPlatformSpecifics(info SystemInfo) SystemInfo {
+	info.Name = "UNKNOWN"
+	info.Version = "UNKNOWN"
+	info.Model = "UNKNOWN"
+	info.CPU = "UNKNOWN"
+	info.Memory = -1
+
 	out, err := exec.Command(
 		"powershell",
 		"-NoProfile",
@@ -14,17 +20,46 @@ func getPlatformSpecifics(info SystemInfo) SystemInfo {
 		"$(Get-WmiObject Win32_OperatingSystem) | ConvertTo-json -Depth 1",
 		"}",
 	).Output()
-	if err != nil {
-		info.Name = "UNKNOWN"
-		info.Version = "UNKNOWN"
-		return info
-	}
-	var st interface{}
-	json.Unmarshal([]byte(out), &st)
-	osMap := st.(map[string]interface{})
+	if err == nil {
+		var st interface{}
+		json.Unmarshal([]byte(out), &st)
+		osMap := st.(map[string]interface{})
 
-	info.Name = osMap["Caption"].(string)
-	info.Version = osMap["Version"].(string)
+		info.Name = osMap["Caption"].(string)
+		info.Version = osMap["Version"].(string)
+	}
+
+	out, err = exec.Command(
+		"powershell",
+		"-NoProfile",
+		"-Command",
+		"&{",
+		"$(Get-WmiObject Win32_Processor) | ConvertTo-json -Depth 1",
+		"}",
+	).Output()
+	if err == nil {
+		var st interface{}
+		json.Unmarshal([]byte(out), &st)
+		procMap := st.(map[string]interface{})
+		info.CPU = procMap["Name"].(string)
+	}
+
+	out, err = exec.Command(
+		"powershell",
+		"-NoProfile",
+		"-Command",
+		"&{",
+		"$(Get-WmiObject Win32_ComputerSystem) | ConvertTo-json -Depth 1",
+		"}",
+	).Output()
+	if err == nil {
+		var st interface{}
+		json.Unmarshal([]byte(out), &st)
+		csMap := st.(map[string]interface{})
+
+		info.Model = csMap["Manufacturer"].(string) + " " + csMap["Model"].(string)
+		info.Memory = int64(csMap["TotalPhysicalMemory"].(float64))
+	}
 
 	return info
 }
